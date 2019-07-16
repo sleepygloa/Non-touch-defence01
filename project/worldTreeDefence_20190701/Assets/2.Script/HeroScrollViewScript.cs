8e57b821871051ab0964d87844141cccf41f2e2c
sleepygloa <sleepygloa@gmail.com>
2019-07-16 23:20:47 +0900

    클릭이벤트 기능 구현

---
+++ project/worldTreeDefence_20190701/Assets/2.Script/HeroScrollViewScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScrollViewScript : MonoBehaviour
+{
+
+
+
+    public void Open() { gameObject.SetActive(true); Debug.Log('d'); }
+
+    public void Close() { gameObject.SetActive(false); }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
